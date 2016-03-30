c709bd16e730ff841278938c9d3978d47aa4b4ae
Jefferson Holbrook <holbrook002@gmail.com>
2016-03-30 16:32:30 -0400

    Basic implementation of arithmetic is finished;  All basic tests pass now.

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,17 +1,74 @@
 package Calculator;
 
 
+import java.util.Objects;
+
 public class Calculator {
 
-    public Calculator(){
+
+    public Calculator() {
 
     }
 
-    public static Double calculate(String str) {
-
-
-
-        return 0.0;
+    public String[] parseInputString(String str) {
+        return str.split("\\s+");
     }
+
+    public double calculation(String str) {
+        String[] parsedInput = str.split("\\s+");
+        return calculate(0.0, parsedInput);
+
+    }
+
+    private double calculate(double result, String[] expr) {
+
+        if(expr.length <= 3){
+            switch(expr[1]){
+                case "*":
+                    result = multiply(Double.parseDouble(expr[0]), Double.parseDouble(expr[2]));
+                    break;
+                case "/":
+                    result = divide(Double.parseDouble(expr[0]), Double.parseDouble(expr[2]));
+                    break;
+                case "+":
+                    result = add(Double.parseDouble(expr[0]), Double.parseDouble(expr[2]));
+                    break;
+                case "-":
+                    result = subtract(Double.parseDouble(expr[0]), Double.parseDouble(expr[2]));
+                    break;
+            }
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for(int i = 1; i < expr.length; i++){
+                switch(expr[i]){
+                    case "*":
+                    case "/":
+
+                        calculate(result, expr);
+                    case "+":
+                    case "-":
+                }
+            }
+            result += calculate(result, expr);
+        }
+        return result;
+    }
+
+    public double add(double lhs, double rhs) {
+        return lhs + rhs;
+    }
+
+    public double subtract(double lhs, double rhs) {
+        return lhs - rhs;
+    }
+
+    public double multiply(double lhs, double rhs) {
+        return lhs * rhs;
+    }
+
+    public double divide(double lhs, double rhs) {
+        return lhs / rhs;
+    }
+
 
 }
--- src/Calculator/CalculatorTest.java
+++ src/Calculator/CalculatorTest.java
@@ -1,7 +1,65 @@
 package Calculator;
 
-/**
- * Created by tuf27107 on 3/30/2016.
- */
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class CalculatorTest {
+
+    @Test
+    public void testAdd(){
+        Calculator calc = new Calculator();
+        double expected = 5;
+        double actual = calc.add(3, 2);
+
+        assertEquals("3+2 should equal 5", expected, actual, 0.0);
+    }
+
+    @Test
+    public void testSubtract(){
+        Calculator calc = new Calculator();
+        double expected = 10;
+        double actual = calc.subtract(15, 5);
+
+        assertEquals("15-5 should equal 10", expected, actual, 0.0);
+    }
+
+    @Test
+    public void testMulitply(){
+        Calculator calc = new Calculator();
+        double expected = 6;
+        double actual = calc.multiply(3, 2);
+
+        assertEquals("3*2 should equal 6", expected, actual, 0.0);
+    }
+
+    @Test
+    public void testDivide(){
+        Calculator calc = new Calculator();
+        double expected = 2.5;
+        double actual = calc.divide(7.5, 3);
+
+        assertEquals("7.5/3 should equal 2.5", expected, actual, 0.0);
+    }
+
+    @Test
+    public void testInputStringParse(){
+        Calculator calculator = new Calculator();
+
+        String[] expected = {"1", "+", "1"};
+        String[] actual = calculator.parseInputString("1 + 1");
+        for(int i = 0; i < 3; i++) {
+            assertEquals("Input \"1 + 1\"' should return {\"1\", \"+\", \"1\"}", expected[i], actual[i]);
+        }
+    }
+
+
+    @Test
+    public void testCalculation(){
+        Calculator calc = new Calculator();
+
+        String input = "3 * 1";
+        double expected = 3.0;
+        double actual = calc.calculation(input);
+    }
+
 }
