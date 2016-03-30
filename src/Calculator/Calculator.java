5eaf3d4d6fcdfb1010367a4b26d518b586332b4e
Jefferson Holbrook <holbrook002@gmail.com>
2016-03-30 14:51:46 -0400

    Inital Commit

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,17 @@
+package Calculator;
+
+
+public class Calculator {
+
+    public Calculator(){
+
+    }
+
+    public static Double calculate(String str) {
+
+
+
+        return 0.0;
+    }
+
+}
---
+++ src/Calculator/CalculatorTest.java
@@ -0,0 +1,7 @@
+package Calculator;
+
+/**
+ * Created by tuf27107 on 3/30/2016.
+ */
+public class CalculatorTest {
+}
